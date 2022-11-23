01e6104fe99c3e42e5892ab3dcbf5ce826348b0d
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-11-23 16:05:28 -0300

    Funções

---
+++ src/Ex01.java
@@ -0,0 +1,14 @@
+public class Ex01 {
+
+    static void soma(int x, int y) {
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+
+    public static void main(String[] args) {
+        soma(10, 5);
+    }
+
+}
