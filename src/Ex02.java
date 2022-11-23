951f922e9dc46584ecc5d40877157e8b0f05a001
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-11-23 18:41:43 -0300

    Argumentos e Parâmetros

---
+++ src/Ex02.java
@@ -0,0 +1,11 @@
+public class Ex02 {
+
+    public static void arr(String[] arr) {
+        System.out.println(arr[0]);
+    }
+
+    public static void main(String[] args) {
+        String[] nome = new String[] { "Nico", "ferca" };
+        arr(nome);
+    }
+}
