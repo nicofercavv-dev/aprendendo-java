fd743c045a8abae840e9327b571cfa99dde8f94c
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-11-24 15:25:43 -0300

    Tipos não-primitivos

---
+++ src/JavaInterativo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JavaInterativo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String name = scanner.nextLine();
+        System.out.println("Ola, " + name);
+    }
+}
---
+++ src/TiposNPrimitivos.java
@@ -0,0 +1,8 @@
+public class TiposNPrimitivos {
+
+    static public void main(String[] args) {
+        int x = 2; //primitivo
+        Integer y = 4; //nao-primitivo
+        System.out.println(x + y);
+    }
+}
