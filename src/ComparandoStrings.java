7209e0574b6bf1dd43710088f60de3d5d0319e00
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-11-29 17:25:32 -0300

    Compara strings

--- src/ComparandoStrings.java
+++ src/ComparandoStrings.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class ComparandoStrings {
 
     public static void main(String[] args) {
         String password = "123456";
+        System.out.println("Digite sua senha: ");
+        Scanner scanner = new Scanner(System.in);
+        String pass = scanner.nextLine();
+        System.out.println(pass.equals(password)); // sempre use equals para comparar strings
+
+        // == nao compara conteudo, compara referencia dos objetos
+        System.out.println(password == pass);
     }
 }
